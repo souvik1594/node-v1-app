f1c2507df3f417bed2ee4a25926798d185bd3b6c
Souvik Kolay <souvik.kolay.1594@gmail.com>
2023-07-10 02:14:49 +0530

    vercel 2

--- controller/user.js
+++ controller/user.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
-const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const path = require("path");
+const data = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "../public/data.json"), "utf-8")
+);
 const users = data.users;
 //Creating Functions
+
 exports.createProduct = (req, res) => {
   console.log(req.body);
   users.push(req.body);
